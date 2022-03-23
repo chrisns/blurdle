2d03e89df7fc157cd75e7a497849c90034633f33
Chris Nesbitt-Smith <chris@cns.me.uk>
2022-03-23 00:23:37 +0000

    add ga

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import "@/assets/base.scss";
+import VueGtag from "vue-gtag";
 
 import vuetify from "./plugins/vuetify";
 
@@ -9,6 +10,8 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(vuetify);
-
+app.use(VueGtag, {
+  config: { id: "G-3YGKVETY1F" },
+});
 
 app.mount("#app");
